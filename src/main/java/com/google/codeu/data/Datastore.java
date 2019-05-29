80648aad86df1c39c92fbb761970289a779749b0
Byambaa0325 <byambaa0325@gmail.com>
2019-05-30 01:32:17 +0800

    Fixed typo

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -71,7 +71,7 @@ public class Datastore {
   */
   public List<Message> getAllMessages(){
 
-    Qurey query =
+    Query query =
     new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
     PreparedQuery results = datastore.prepare(query);
 
