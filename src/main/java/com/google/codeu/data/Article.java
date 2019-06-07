a1018185a1a2a048d5b4a9f4fe784cb430c38879
Byambaa0325 <byambaa0325@gmail.com>
2019-06-07 17:52:43 +0800

    Turn incoming comma seperated string to list

--- src/main/java/com/google/codeu/data/Article.java
+++ src/main/java/com/google/codeu/data/Article.java
@@ -16,66 +16,67 @@
 
 package com.google.codeu.data;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 
-
-/** A single article posted. */
+/**
+ * A single article posted.
+ */
 public class Article {
-  private UUID id;
-  private List<String> authors;
-  private List<String> tags;
-  private String header;
-  private String body;
-  private long timestamp;
+    private UUID id;
+    private List<String> authors;
+    private List<String> tags;
+    private String header;
+    private String body;
+    private long timestamp;
 
-  public Article(String authors, String header, String body){
-    this(UUID.randomUUID(), authors,"", header, body, System.currentTimeMillis());
-  }
+    public Article(String authors, String header, String body) {
+        this(UUID.randomUUID(), authors, "", header, body, System.currentTimeMillis());
+    }
 
-  public Article(UUID id, String authors,String tags, String header, String body, long timestamp) {
-    this.id = id;
-    this.authors = authors.replace(" ","").split(",");
-    this.tags = tags.replace(" ","").split(",");
-    this.header = header;
-    this.body = body;
-    this.timestamp = timestamp;
-  }
+    public Article(UUID id, String authors, String tags, String header, String body, long timestamp) {
+        this.id = id;
+        this.authors = Arrays.asList(authors.replace(" ", "").split(","));
+        this.tags = Arrays.asList(tags.replace(" ", "").split(","));
+        this.header = header;
+        this.body = body;
+        this.timestamp = timestamp;
+    }
 
-  public UUID getId() {
-    return id;
-  }
+    public UUID getId() {
+        return id;
+    }
 
-  public String getAuthors() {
+    public String getAuthors() {
 
-    String authorsCommaSeparated = String.join(",", authors);
-    return authorsCommaSeparated;
-  }
+        String authorsCommaSeparated = String.join(",", authors);
+        return authorsCommaSeparated;
+    }
 
-  public List<String> getAuthorsList() {
-    return authors;
-  }
+    public List<String> getAuthorsList() {
+        return authors;
+    }
 
-  public String getTags(){
+    public String getTags() {
 
-    String tagsCommaSeparated = String.join(",", tags);
+        String tagsCommaSeparated = String.join(",", tags);
 
-    return tagsCommaSeparated;
-  }
+        return tagsCommaSeparated;
+    }
 
-  public String getHeader() {
-    return header;
-  }
+    public String getHeader() {
+        return header;
+    }
 
-  public String getBody() {
-    return body;
-  }
+    public String getBody() {
+        return body;
+    }
 
-  public long getTimestamp() {
-    return timestamp;
-  }
-
+    public long getTimestamp() {
+        return timestamp;
+    }
 
 
 }
