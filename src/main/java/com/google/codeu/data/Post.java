f21e73bfd5a2c990971deff1fb08ee96793e3a27
Byambaa0325 <byambaa0325@gmail.com>
2019-06-30 13:34:39 +0800

    Add Post Class: article class extended to include comments

--- src/main/java/com/google/codeu/data/Post.java
+++ src/main/java/com/google/codeu/data/Post.java
@@ -1,17 +1,37 @@
 package com.google.codeu.data;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Post extends Article {
+    private List<Message> comments;
+
     public Post(String authors, String header, String body) {
         super(authors, header, body);
+        comments = new ArrayList<>();
     }
 
     public Post(String authors, String tags, String header, String body) {
         super(authors, tags, header, body);
+        comments = new ArrayList<>();
     }
 
     public Post(UUID id, String authors, String tags, String header, String body, long timestamp) {
         super(id, authors, tags, header, body, timestamp);
+        comments = new ArrayList<>();
     }
+
+    public List<Message> getComments() {
+        return comments;
+    }
+
+    public void addComment(Message message){
+        comments.add(message);
+    }
+
+    public void deleteComment(Message message){
+        comments.remove(message);
+    }
+
 }
