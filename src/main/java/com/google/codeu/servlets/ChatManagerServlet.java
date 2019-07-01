2a8d8d68cf353dbd8027263faa1e45418e2bf663
Battulga Enkhbayar <tugushka11@gmail.com>
2019-07-01 17:11:48 +0800

    Deleted a package that never used I realy don't know why this came in.

--- src/main/java/com/google/codeu/servlets/ChatManagerServlet.java
+++ src/main/java/com/google/codeu/servlets/ChatManagerServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.files.FileServicePb.ListDirRequest;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
