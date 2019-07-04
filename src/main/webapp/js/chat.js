4ad81ea8938b61cf8c5737f01a2b20a121f46ad3
Battulga Enkhbayar <tugushka11@gmail.com>
2019-07-04 19:47:14 +0800

    Modified buildChatDom Now it returns dom to callback function

--- src/main/webapp/js/chat.js
+++ src/main/webapp/js/chat.js
@@ -35,7 +35,11 @@ function buildConversations(){
       conversatoinDom.innerHTML = '';
       for( let conv of data ){
         conversatoinDom.appendChild( buildConversationDom(conv) );
-        chatDom.appendChild( buildChatDom(conv) );
+        buildChatDom( conv.id, dom => {
+          chatDom.appendChild(dom);
+          loadChat(conv.id);
+        })
+        // chatDom.appendChild( buildChatDom(conv.id) );
         console.log( 'Created :', conv.nickname );
       }
     });
@@ -57,22 +61,27 @@ function buildConversationDom( conv ){
   return dom;
 }
 
-function buildChatDom( conv ){
-  let dom = document.createElement('div');
-  dom.classList.add('chat');
-  dom.style.display = 'none';
-  dom.id = `chat-${conv.id}`;
-  dom.innerHTML = `
-    <div style="height: 760px; overflow-y: scroll;">
-      <div class="chat-header">${conv.nickname}</div>
-      <div class="chat-container">Loading...</div>
-    </div>
-    <div id="message-input">
-      <input type="text" id="message-${conv.id}">
-      <button onclick="sendMessage('${conv.id}')">Send</button>
-    </div>
-  `;
-  return dom;
+function buildChatDom( convid, callback ){
+  fetch( `/chat/get/conversation/?convid=${convid}` )
+    .then( resp => resp.json() )
+    .then( conv => {
+      let dom = document.createElement('div');
+      console.log("Okay");
+      dom.classList.add('chat');
+      dom.style.display = 'none';
+      dom.id = `chat-${conv.id}`;
+      dom.innerHTML = `
+        <div style="height: 760px; overflow-y: scroll;">
+          <div class="chat-header">${conv.nickname}</div>
+          <div class="chat-container">Loading...</div>
+        </div>
+        <div id="message-input">
+          <input type="text" id="message-${conv.id}">
+          <button onclick="sendMessage('${conv.id}')">Send</button>
+        </div>
+      `;
+      callback(dom);
+    });
 }
 
 function loadChat( id ){
