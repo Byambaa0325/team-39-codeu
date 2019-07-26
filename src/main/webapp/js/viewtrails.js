2d41434d942707fd1f7f4713eb603dae116358df
Byambaa <Byambaa0325@gmail.com>
2019-07-26 14:36:32 +0800

    Comments on viewtrails js

--- src/main/webapp/js/viewtrails.js
+++ src/main/webapp/js/viewtrails.js
@@ -13,30 +13,40 @@ $(window).load(function(){
           north: 85,
           south: -85,
           west: -180,
-          east: 180    }
+          east: 180
+          }
         }
       };
 
       map = new google.maps.Map(document.getElementById('mapFull'), mapOptions);
       fetchTrails();
 
+      /* Attach geolocater to map's dragend event to track where user's viewing location*/
       map.addListener('dragend', function() {
+        //Get center coordinate
         var latlng = map.getCenter().toJSON();
+        //Send reverse-geolocation request
         geocoder.geocode({'location': latlng}, function(results, status) {
           if (status === 'OK') {
+            //update several elements with country name
             if (results[0]) {
               var currentLocationElement = document.getElementById('current-location');
               var currentLocationForumLink = document.getElementById('side-panel-forumlink');
               var country = ""
+
+              //Find country name from components of address returned
               results[0].address_components.forEach((component) => {
                 if (component.types.toString().includes('country')){
                   country = component.long_name;
                 }
               });
+
+              //if we are in a country
               if(country != ""){
                 currentLocationElement.innerHTML = "<a href = \"forum?country="+country.toLowerCase()+"\"><h1>"+country+"</h1></a>";
                 currentLocationForumLink.innerHTML = "<a href = \"forum?country="+country.toLowerCase()+"\" style=\"color:darkslateblue\" ><em>"+"View Discussion in "+country+"</em></a>";
               }
+              //update forum by the country
               fetchForum();
             } else {
               console.log('No results found');
@@ -45,18 +55,24 @@ $(window).load(function(){
             console.log('Geocoder failed due to: ' + status);
           }
         });
-
       });
     }
+
     function fetchTrails(){
       let loaderStatus = true;
+      //Toggle loader on the page
       toggleLoader(loaderStatus, 'mapFull', 'loadDiv');
       fetch('/articles').then((response) => {
         return response.json();
       }).then((articles) => {
         articles.forEach((article) => {
+
+          //only create markers for articles with trails
           if(article.coordinates.length != 0){
             createTrailForDisplay(article.authors, article.tags, article.header, article.body, article.coordinates, article.id);
+
+            //After creating marker on map if loader is still on disable it
+            //P.S: This is mildly inefficient since it needs to trigger only once but is checked on every trail
             if(loaderStatus){
               loaderStatus = false;
               toggleLoader(loaderStatus, 'mapFull', 'loadDiv');
@@ -92,20 +108,24 @@ $(window).load(function(){
 
       var containerDiv = pathInfo(authors, tags, header, body, id);
 
+      //Hide trail on click on the trail
       poly.addListener('click',function(){
         poly.setMap(null);
       });
 
+      //infowindow on hovering a marker
       marker.addListener('mouseover', function(){
         infowindow.setContent(containerDiv.outerHTML);
         infowindow.setPosition(startingPoint);
         infowindow.open(map);
       });
-      marker.addListener('mouseout', function(){
 
+      //close the infowindow opened by hover
+      marker.addListener('mouseout', function(){
         infowindow.close();
       });
 
+      //Set details on pane and pan to the markers location.
       marker.addListener('click', function(){
         poly.setMap(map);
         toggleArticleOnArticlePanel(id);
